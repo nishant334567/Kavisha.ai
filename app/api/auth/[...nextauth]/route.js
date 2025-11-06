281375623d5ae6d6dacfe0eb1c3cefddaf7b2bae
Nishant Kumar <nishantkumar334567@gmail.com>
2025-11-07 00:39:10 +0530

    removed checks

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -22,6 +22,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      checks: ["none"],
     }),
   ],
 
